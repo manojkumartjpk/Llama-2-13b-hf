cb6eb42689c99341d350ae531b519651e0ea1efe
karthik1900 <40236661+karthik1900@users.noreply.github.com>
2024-06-24 16:31:58 +0530

    Update input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -4,5 +4,11 @@ INPUT_SCHEMA = {
         'required': True,
         'shape': [1],
         'example': ["How to make a omellete ?"]
+    },
+    'chat_history': {
+        'datatype': 'STRING',
+        'required': False,
+        'shape': [-1,2],
+        'example': ["How to make a omellete ?", "How to make a omellete ?]
     }
 }
